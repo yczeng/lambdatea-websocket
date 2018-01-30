1c2e8d010df1ed5a55ec697cd06069d69b9aea42
Ubuntu <ubuntu@ip-172-31-27-32.us-west-2.compute.internal>
2018-01-30 08:52:18 +0000

    working websocket

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from multiprocessing import Process, Queue
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	#server.send_message_to_all("Hey all, a new client has joined us")
 
 
 # Called for every client disconnecting
@@ -24,8 +24,15 @@ def client_left(client, server):
 # Called when a client sends a message
 def message_received(client, server, message):
 	print("Client(%d) said: %s" % (client['id'], message))
-	server.send_message(client, "hi")	
-	
+	old_balance = float(os.popen('venmo balance').read().strip('\n'))
+
+	venmo_waiting = True
+	while venmo_waiting:
+		new_balance = float(os.popen('venmo balance').read().strip('\n'))
+		
+		if new_balance - old_balance >= 0.01:
+			server.send_message(client, "uramazing. emailusat hello@lambdatea.com")
+			venmo_waiting = False
 
 PORT=9001
 HOST="172.31.27.32"
@@ -35,7 +42,6 @@ server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
 
 def runserver():
-	print("howdy")
 	server.run_forever()
 
 def venmo():
@@ -46,5 +52,5 @@ def venmo():
 first = Process(target=runserver, args=())
 second = Process(target=venmo, args=())
 
-second.start()
 first.start()
+second.start()
